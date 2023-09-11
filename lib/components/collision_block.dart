bcbd5a164c4e9415cbfd26d764da770f7f182606
Amr Elsaadny <a.alsaadny@najdahksa.com>
2023-09-12 02:50:04 +0300

    disable debug

--- lib/components/collision_block.dart
+++ lib/components/collision_block.dart
@@ -13,6 +13,6 @@ class CollisionBlock extends PositionComponent {
     required Vector2 size,
     this.isPlatform = false,
   }) : super(position: position, size: size) {
-    debugMode = true;
+    // debugMode = true;
   }
 }
